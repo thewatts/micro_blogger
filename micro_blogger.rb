4853fe8951068db80038891bb902c41a3ddb5791
Nathaniel Watts <reg@nathanielwatts.com>
2013-09-13 07:46:50 -0600

    cleanup

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -21,29 +21,29 @@ class MicroBlogger
 
   def run
     puts "Welcome to the JSL Twitter Client!"
-    
+
     input = ""
     while input != "q"
       puts ""
       printf "enter command: "
-      input = gets.chomp
-      parts = input.split(" ")
+      input   = gets.chomp
+      parts   = input.split(" ")
       command = parts[0]
       process_command(command, parts)
     end
   end
 
-  def process_command(command, parts)    
+  def process_command(command, parts)
     case command
-      when "q" then puts "Goodbye!"
-      when "s" then puts shorten_url(parts[1])
-      when "t" then tweet(parts[1..-1].join(" "))
+      when "q"     then puts "Goodbye!"
+      when "s"     then puts shorten_url(parts[1])
+      when "t"     then tweet(parts[1..-1].join(" "))
       when "klout" then klout_score
-      when "turl" then tweet_with_url(parts[1..-1])
-      when "dm" then dm(parts[1], parts[2..-1].join(" "))
-      when "spam" then spam_my_followers(parts[1..-1].join(" "))
-      when "elt" then everyones_last_tweet
-      when "help" then puts help
+      when "turl"  then tweet_with_url(parts[1..-1])
+      when "dm"    then dm(parts[1], parts[2..-1].join(" "))
+      when "spam"  then spam_my_followers(parts[1..-1].join(" "))
+      when "elt"   then everyones_last_tweet
+      when "help"  then puts help
     else
       puts "Sorry, I don't know to #{command}"
     end
@@ -54,7 +54,7 @@ class MicroBlogger
     == DESCRIPTION
 
     MicroBlogger is a terminal Twitter Client,
-    built by Brian Winterling and Nathaniel Watts  
+    built by Brian Winterling and Nathaniel Watts
 
     == COMMAND SYNTAX
     command content_1 content_2 (no special characters required)
@@ -62,7 +62,7 @@ class MicroBlogger
     == COMMANDS
     t message          :: Tweet out a message, using your Twitter Account!
     dm user message    :: Tweet out a direct message to a specific user
-    turl message       :: Tweet out a message and shorten any url with Bit.ly 
+    turl message       :: Tweet out a message and shorten any url with Bit.ly
     spam message       :: Tweet a direct message to all of your friends
     elt                :: Return a list of your friends' last tweets
     s url              :: Shorten a URL with Bit.ly
@@ -75,7 +75,7 @@ class MicroBlogger
   def tweet(message)
     unless message.length > 140 || message.empty?
       @client.update(message)
-    else 
+    else
       puts message.empty? ? "Error: you need to add a message." : "ERROR: your message is over 140 chars"
     end
   end
@@ -91,14 +91,14 @@ class MicroBlogger
 
   def tweet_with_url(parts)
     parts.collect! { |word| uri?(word) ? shorten_url(word) : word}
-    tweet(parts.join(" ")) 
+    tweet(parts.join(" "))
   end
 
   def dm(target, message)
     puts "Trying to send #{target} this direct message:"
     puts message
 
-    unless target.nil? || message.empty?  
+    unless target.nil? || message.empty?
       if get_followers.include?(target)
         direct_message = "d #{target} #{message}"
         tweet(direct_message)
@@ -106,8 +106,8 @@ class MicroBlogger
         puts "#{target} hates you... stalker."
       end
     else
-      puts "ERROR: Missing user name or message." 
-    end 
+      puts "ERROR: Missing user name or message."
+    end
   end
 
   def spam_my_followers(message)
@@ -117,7 +117,7 @@ class MicroBlogger
   end
 
   def get_followers
-    screen_names = @client.followers.collect do |follower| 
+    screen_names = @client.followers.collect do |follower|
       follower.screen_name
     end
   end
@@ -127,7 +127,7 @@ class MicroBlogger
     friends.sort_by! { |friend| friend.screen_name.downcase }
     friends.each do |friend|
       last_tweet = friend.status.text
-      timestamp = friend.status.created_at
+      timestamp  = friend.status.created_at
       print "#{friend.screen_name} just said... "
       print last_tweet
       print " published #{timestamp.strftime('%A, %b %d')}"
@@ -154,8 +154,8 @@ class MicroBlogger
     friends.each do |friend|
       begin
         identity = Klout::Identity.find_by_screen_name(friend)
-        user = Klout::User.new(identity.id)
-        score = user.score.score.to_s
+        user     = Klout::User.new(identity.id)
+        score    = user.score.score.to_s
         print "#{friend}'s score: #{score[0..2]}"
       rescue
         print "Sorry, #{friend} doesn't have a Klout account"
@@ -163,7 +163,6 @@ class MicroBlogger
       puts "" # Print a blank line to separate each friend
     end
   end
-
 end
 
 blogger = MicroBlogger.new
